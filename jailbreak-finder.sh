91a2fcbba61d942d6961fe1b16770b363252de22
Cocoa Oikawa <0xbbc@0xbbc.com>
2018-02-08 11:50:25 +0800

    macOS support

--- jailbreak-finder.sh
+++ jailbreak-finder.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 clear
-
-wlanmax=$(ifconfig wlan0 | grep "inet addr:" | cut -d: -f2 | awk '{print $1};' | awk -F. '{print $1"."$2"."$3".1/24"};')
+PLATFORM=`uname`
+case ${PLATFORM} in
+    Darwin*)
+        wlanmax=$(ifconfig en0 | grep "inet " | cut -d' ' -f2 | awk '{print $1};' | awk -F. '{print $1"."$2"."$3".1/24"};')
+        ;;
+    *)
+        wlanmax=$(ifconfig wlan0 | grep "inet addr:" | cut -d: -f2 | awk '{print $1};' | awk -F. '{print $1"."$2"."$3".1/24"};')
+        ;;
+esac
 echo "Grabbing inet address..."
 echo "Grabbing gatemask..."
 nmap -sP $wlanmax | grep "Nmap scan report for" | awk '{system("ssh -t -t -o ConnectTimeout=5 root@"$5)};'
